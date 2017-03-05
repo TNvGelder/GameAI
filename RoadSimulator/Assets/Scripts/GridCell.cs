1049029cbb115c8ec850e9a6c799536b7aef95f8
JeroenVinke <jeroenvinke17@gmail.com>
2017-03-05 18:40:13 +0100

    no message

--- RoadSimulator/Assets/Scripts/GridCell.cs
+++ RoadSimulator/Assets/Scripts/GridCell.cs
@@ -17,69 +17,44 @@ namespace Assets.Scripts
     {
         public float x { get; set; }
         public float y { get; set; }
-        public static int Width = 10;
-        public static int Height = 10;
+        public static int Width = 20;
+        public static int Height = 20;
 
         public GameObjectType Type { get; set; }
 
-        public Vector2D[] GetVertices()
+        public GridCell()
         {
-            return new Vector2D[]
-            {
-                //new Vector2D(x - Width / 2, y - Height / 2),
-                //new Vector2D(x + Width / 2, y - Height / 2),
-                //new Vector2D(x + Width / 2, y + Height / 2),
-                //new Vector2D(x - Width / 2, y + Height / 2)
-
-                new Vector2D(x, y),
-                new Vector2D(x + Width - 5, y),
-                new Vector2D(x, y + Height - 5),
-                new Vector2D(x + Width - 5, y + Height - 5),
-
-            };
+            Type = GameObjectType.Grass;
         }
 
         internal void Render()
         {
-            var v = GetVertices();
+            Color color = new Color();
 
-            //var cellPos = Camera.main.WorldToScreenPoint(new Vector3(v[0].X, v[0].Y, 0));
+            switch(Type)
+            {
+                case GameObjectType.Grass:
+                    color = new Color(0, 255, 0, 0.3f);
+                    break;
+                case GameObjectType.Shop:
+                    color = new Color(255, 0, 0, 0.3f);
+                    break;
+                case GameObjectType.Road:
+                    color = new Color(0, 0, 0, 0.3f);
+                    break;
+            }
 
-            //GL.Vertex3(0, 0, -99);
-            //GL.Vertex3(10, 10, -99);
+            var texture = new Texture2D(Width, Height);
+            for (int y = 0; y < texture.height; ++y)
+            {
+                for (int x = 0; x < texture.width; ++x)
+                {
+                    texture.SetPixel(x, y, color);
+                }
+            }
+            texture.Apply();
 
-            //var V = Camera.main.WorldToScreenPoint(new Vector2(v[0].X, v[0].Y));
-            //GL.Vertex3(V.x, V.y, V.z);
-            //V = Camera.main.WorldToScreenPoint(new Vector2(v[1].X, v[1].Y));
-            //GL.Vertex3(V.x, V.y, V.z);
-            //V = Camera.main.WorldToScreenPoint(new Vector2(v[2].X, v[2].Y));
-            //GL.Vertex3(V.x, V.y, V.z);
-            //V = Camera.main.WorldToScreenPoint(new Vector2(v[3].X, v[3].Y));
-            //GL.Vertex3(V.x, V.y, V.z);
-
-            //GL.Vertex3((v[0].X) / Screen.width, (v[0].Y) / Screen.height, -99);
-            //GL.Vertex3((v[1].X) / Screen.width, (v[1].Y) / Screen.height, -99);
-            //GL.Vertex3((v[2].X) / Screen.width, (v[2].Y) / Screen.height, -99);
-            //GL.Vertex3((v[3].X) / Screen.width, (v[3].Y) / Screen.height, -99);
-
-            
-            //GL.Vertex3((v[0].X) / Screen.width, (v[0].Y) / Screen.height, -99);
-            //GL.Vertex3((v[1].X) / Screen.width, (v[1].Y) / Screen.height, -99);
-            //GL.Vertex3((v[2].X) / Screen.width, (v[2].Y) / Screen.height, -99);
-            //GL.Vertex3((v[3].X) / Screen.width, (v[3].Y) / Screen.height, -99);
-
-
-            GUI.Box(new Rect(new Vector2(x, y), new Vector2(Width, Height)), "");
-
-            //GL.Vertex(v[0].ToVector2());
-            //GL.Vertex(v[1].ToVector2());
-            //GL.Vertex(v[2].ToVector2());
-            //GL.Vertex(v[3].ToVector2());
-
-            //GL.Vertex3((v[0].X), (v[0].Y), -99);
-            //GL.Vertex3((v[1].X), (v[1].Y), -99);
-            //GL.Vertex3((v[2].X), (v[2].Y), -99);
-            //GL.Vertex3((v[3].X), (v[3].Y), -99);
+            GUI.Box(new Rect(new Vector2(x, y), new Vector2(Width, Height)), texture);
         }
     }
 }
--- RoadSimulator/Assets/Scripts/World.cs
+++ RoadSimulator/Assets/Scripts/World.cs
@@ -18,6 +18,7 @@ public class World : MonoBehaviour {
     public float Height { get; set; }
     public int MinDetectionBoxLength { get; internal set; }
     Graph<GameNode> graph = new Graph<GameNode>();
+    public bool ShouldRenderGrid = true;
 
     public Car Target;
 
@@ -29,35 +30,39 @@ public class World : MonoBehaviour {
         Width = Height * Screen.width / Screen.height;
         MinDetectionBoxLength = 5;
 
-        //GameObject carGameObjects = GameObject.Find("Cars");
-        //foreach(Transform child in carGameObjects.transform)//Gets direct children of cars
-        //{
-        //    GameObject obj = child.gameObject;
-        //    SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
-
-        //    //Gets size of the sprite in units
-        //    Vector2D size = new Vector2D(spriteRenderer.bounds.size);
-        //    Vector2D pos = new Vector2D(child.position);
-        //    Car car = new Car(obj, pos, size, this);
-        //    car.BRadius = car.Size.Y;
-        //    cars.Add(car);
-        //    entities.Add(car);
-        //    car.SteeringBehaviours.Add(new ObstacleAvoidanceBehavior(car));
-        //}
-        //if (cars.Count > 1)
-        //{
-        //    Target = cars[0];
-        //    Target.SteeringBehaviours.Add(new FleeBehaviour(Target, cars[1]));
-
-        //    for (int i = 1; i < cars.Count; i++)
-        //    {
-        //        Car car = cars[i];
-        //        car.SteeringBehaviours.Add(new SeekBehaviour(car, Target));
-        //    }
-        //}
-
+        //SetupCars();
         GenerateGraph();
         GenerateGrid();
+    }
+
+    private void SetupCars()
+    {
+        GameObject carGameObjects = GameObject.Find("Cars");
+        foreach (Transform child in carGameObjects.transform)//Gets direct children of cars
+        {
+            GameObject obj = child.gameObject;
+            SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
+
+            //Gets size of the sprite in units
+            Vector2D size = new Vector2D(spriteRenderer.bounds.size);
+            Vector2D pos = new Vector2D(child.position);
+            Car car = new Car(obj, pos, size, this);
+            car.BRadius = car.Size.Y;
+            cars.Add(car);
+            entities.Add(car);
+            car.SteeringBehaviours.Add(new ObstacleAvoidanceBehavior(car));
+        }
+        if (cars.Count > 1)
+        {
+            Target = cars[0];
+            Target.SteeringBehaviours.Add(new FleeBehaviour(Target, cars[1]));
+
+            for (int i = 1; i < cars.Count; i++)
+            {
+                Car car = cars[i];
+                car.SteeringBehaviours.Add(new SeekBehaviour(car, Target));
+            }
+        }
     }
 
     private void GenerateGrid()
@@ -91,92 +96,6 @@ public class World : MonoBehaviour {
             });
         }
 
-        foreach (Transform child in shops.transform)
-        {
-            graph.AddNode(new GameNode()
-            {
-                IsShop = true,
-                x = child.position.x,
-                y = child.position.y
-            });
-        }
-    }
-
-    private void OnGUI()
-    {
-        //Debug.Log("(" + Input.mousePosition.x + ", " + Input.mousePosition.y + ")");
-        RenderNodes();
-        RenderGrid();
-    }
-
-    private void RenderGrid()
-    {
-        //GL.Color(new Color(255, 0, 0));
-        //GL.PushMatrix();
-        //GL.LoadOrtho();
-        //GL.Begin(GL.LINES);
-
-        //GL.Vertex3(0, 1f, 0);
-        //GL.Vertex3(0.5f, 0.5f, 0);
-        //GL.End();
-        //GL.PopMatrix();
-        //return;
-
-        foreach (var cell in Grid.Cells)
-        {
-
-
-            //float a = i / (float)lineCount;
-            //float angle = a * Mathf.PI * 2;
-            // Vertex colors change from red to green
-            // One vertex at transform position
-            cell.Render();
-            // Another vertex at edge of circle
-            //GL.Vertex3(Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius, 0);
-
-            //var size = new Vector2D(5, 5);
-            //var color = new Color(255, 0, 0);
-
-            //Texture2D tex = new Texture2D(2, 2);
-            //for (int i = 0; i < tex.width; i++)
-            //{
-            //    for (int j = 0; j < tex.height; j++)
-            //    {
-            //        tex.SetPixel(i, j, color);
-            //    }
-            //}
-            //tex.Apply();
-            //GUIStyle tempstyle = new GUIStyle();
-            //tempstyle.normal.background = tex;
-
-            //var guiPosition = new Vector2(cell.x, Screen.height - cell.y);
-            //GUILayout.BeginArea(new Rect(guiPosition, size.ToVector2()), tempstyle);
-            //GUILayout.EndArea();
-
-            //GL.End();
-            //GL.PopMatrix();
-            //return;
-        }
-        //GL.End();
-        //GL.PopMatrix();
-    }
-
-    private GameObjectType GetCellType(GridCell cell)
-    {
-        var roads = GameObject.Find("Roads");
-        var shops = GameObject.Find("Shops");
-
-        foreach (var child in GameObject.FindGameObjectsWithTag("Roads"))
-        {
-            var childPos = Camera.main.WorldToScreenPoint(child.transform.position);
-            var bounds = child.GetComponent<Renderer>();
-            if (childPos.x < cell.x && childPos.x + bounds.bounds.size.x > cell.x &&
-                childPos.y < cell.y && childPos.y + bounds.bounds.size.y > cell.y) {
-                return GameObjectType.Road;
-            }
-        }
-
-        return GameObjectType.Grass;
         //foreach (Transform child in shops.transform)
         //{
         //    graph.AddNode(new GameNode()
@@ -186,6 +105,55 @@ public class World : MonoBehaviour {
         //        y = child.position.y
         //    });
         //}
+    }
+
+    private void OnGUI()
+    {
+        //RenderNodes();
+        
+        if (ShouldRenderGrid)
+        {
+            RenderGrid();
+        }
+    }
+
+    private void RenderGrid()
+    {
+        foreach (var cell in Grid.Cells)
+        {
+            cell.Render();
+        }
+    }
+
+    private GameObjectType GetCellType(GridCell cell)
+    {
+        foreach (var child in GameObject.FindGameObjectsWithTag("Roads"))
+        {
+            Vector3[] array = SpriteLocalToWorld(child);
+            
+            var p1 = Camera.main.WorldToScreenPoint(array[0]);
+            var p2 = Camera.main.WorldToScreenPoint(array[1]);
+
+            if (p1.x <= cell.x && p2.x >= cell.x + 20 &&
+                p1.y <= cell.y && p2.y >= cell.y + 20)
+            {
+                return GameObjectType.Road;
+            }
+        }
+
+        return GameObjectType.Grass;
+    }
+
+    Vector3[] SpriteLocalToWorld(GameObject obj)
+    {
+        Sprite sp = obj.GetComponent<SpriteRenderer>().sprite;
+        Vector3 pos = obj.transform.position;
+        Vector3[] array = new Vector3[2];
+        //top left
+        array[0] = pos + sp.bounds.min;
+        // Bottom right
+        array[1] = pos + sp.bounds.max;
+        return array;
     }
 
     private void RenderNodes()
@@ -216,27 +184,15 @@ public class World : MonoBehaviour {
         }
     }
 
-    private void OnPostRender()
-    {
-        //var mousePos = Input.mousePosition;
-        //Vector3 startVertex = new Vector3();
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //    startVertex = new Vector3(mousePos.x / Screen.width, mousePos.y / Screen.height, 0);
-        //GL.PushMatrix();
-        ////mat.SetPass(0);
-        //GL.LoadOrtho();
-        //GL.Begin(GL.LINES);
-        //GL.Color(Color.red);
-        //GL.Vertex(startVertex);
-        //GL.Vertex(new Vector3(mousePos.x / Screen.width, mousePos.y / Screen.height, 0));
-        //GL.End();
-        //GL.PopMatrix();
-    }
-
     void FixedUpdate () {
         if (Input.GetKeyDown("r"))
         {
             SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown("g"))
+        {
+            ShouldRenderGrid = !ShouldRenderGrid;
         }
 
         float timeElapsed = Time.fixedDeltaTime;
